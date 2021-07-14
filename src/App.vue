2745c10b090f887336b3b1c794a52957cd820407
Julien P <julien.pinel@gmail.com>
2021-07-14 21:27:44 +0200

    in progress

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,62 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 in CodeSandbox is nice !" test="gg" />
+  <Grid :table="matrice" />
 </template>
 
 <script>
-import HelloWorldVue from "./components/HelloWorld.vue";
+/*import HelloWorldVue from "./components/HelloWorld.vue";*/
+import Grid from "./components/Grid.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld: HelloWorldVue,
+    /*HelloWorld: HelloWorldVue,*/
+    Grid: Grid,
   },
+  data() {
+    return {
+      matrice: test()
+    }
+  }
 };
+
+var initmatrice = [
+      { id: 1, ligne: [1,2,3,4,5,6,7,8,9] },
+      { id: 2, ligne: [1,2,3,4,5,6,7,8,9] },
+      { id: 3, ligne: [1,2,3,4,5,6,7,8,9] },
+      { id: 4, ligne: [1,2,3,4,5,6,7,8,9] },
+      { id: 5, ligne: [1,2,3,4,5,6,7,8,9] },
+      { id: 6, ligne: [1,2,3,4,5,6,7,8,9] },
+      { id: 7, ligne: [1,2,3,4,5,6,7,8,9] },
+      { id: 8, ligne: [1,2,3,4,5,6,7,8,9] },
+      { id: 9, ligne: [1,2,3,4,5,6,7,8,9] }
+    ]
+
+function test(){
+  let initmatrice1 = shuffle(initmatrice)
+  return initmatrice1
+}
+
+function shuffle(array) {
+  var currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+
+
+
 </script>
 
 <style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="grid">
+    <span v-for="cell in table" :key="cell.id">
+      <span v-for="cell1 in cell.ligne" :key="cell1.id"> {{cell1}}</span>
+    </span>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Grid",
+  props: {
+    msg: String,
+    table: Array,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.grid span{
+    display: Grid;
+    grid-template-columns: repeat(9, 1fr);
+    border: solid 1px black;
+    max-width: 620px;
+}
+.grid span{
+  border: solid .5px gray;
+  background-color: khaki;
+  text-align: center;
+  width: 100%;
+}
+</style>
